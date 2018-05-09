a9876723594e1eb3865e02f58d7288bc12ed0603
Erik Søe Sørensen <ess@trifork.com>
2018-05-10 00:03:17 +0200

    HumanPlayer: accept short notation.

--- Player.py
+++ Player.py
@@ -36,21 +36,41 @@ class HumanPlayer(Player):
             move = self.parseMove(line, board)
             if move in validMoves:
                 return move
+            elif move != None: # Incomplete move - try to complete it:
+                candidates = [m for m in validMoves
+                              if self.moveMatches(move, m)]
+                if len(candidates) == 1:
+                    return candidates[0]
+                else:
+                    sys.stderr.write("Ambiguous move\n")
             else:
                 sys.stderr.write("Invalid move :-(\n")
 
-    def parseMove(self, line, board):
-        m = re.match("^\\s*([a-h])([1-8])\\s*-\\s*([a-h])([1-8])\\s*$",
-                     line)
-        if m==None:
-            return None
+    def moveMatches(self, cand, move):
+        def m(a,b): return a==b or a==-1
+        return (m(cand.src[0], move.src[0]) and
+                m(cand.src[1], move.src[1]) and
+                m(cand.dst[0], move.dst[0]) and
+                m(cand.dst[1], move.dst[1]))
+        
 
+    def parseMove(self, line, board):
         def col(s): return ord(s)-ord("a")
         def row(s): return ord(s)-ord("1")
-        src = (col(m.group(1)), row(m.group(2)))
-        dst = (col(m.group(3)), row(m.group(4)))
-        return Moves.Move(board, src, dst)
-    
+
+        m = re.match("^\\s*([a-h])([1-8])\\s*-\\s*([a-h])([1-8])\\s*$", line)
+        if m!=None:
+            src = (col(m.group(1)), row(m.group(2)))
+            dst = (col(m.group(3)), row(m.group(4)))
+            return Moves.Move(board, src, dst)
+
+        m = re.match("^\\s*([a-h])([1-8])\\s*$", line)
+        if m!=None:
+            src = (-1,-1)
+            dst = (col(m.group(1)), row(m.group(2)))
+            return Moves.Move(board, src, dst)
+        
+        return None
     
 
 
